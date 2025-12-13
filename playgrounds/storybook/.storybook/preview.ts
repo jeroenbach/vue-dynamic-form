d30ae7cede6529d4d5005826e2d8e70fcdffe541
Jeroen Bach <jeroen.bach@gmail.com>
2025-12-13 11:35:45 +0100

    Improved development experience
    - Restored antfu eslint automatic corrections
    - Improved package scripts and version

--- playgrounds/storybook/.storybook/preview.ts
+++ playgrounds/storybook/.storybook/preview.ts
@@ -1,5 +1,12 @@
 import type { Preview } from '@storybook/vue3-vite';
+import { setup } from '@storybook/vue3';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import './input.css';
+
+setup((app) => {
+  app.use(ElementPlus);
+});
 
 const preview: Preview = {
   parameters: {
